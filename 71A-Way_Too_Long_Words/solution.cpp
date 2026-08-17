4be5317499eaa6621008e3ad567f88775ab54e81
Harshith M <mharshith200@gmail.com>
2026-08-17 11:55:04 +0530

    Add solution for 71A (Way Too Long Words)
    Submission: https://codeforces.com/contest/71/submission/347503044

---
+++ 71A-Way_Too_Long_Words/solution.cpp
@@ -0,0 +1,1 @@
+#include <iostream>#include <vector>using namespace std; int main() {    int n;    cin >> n;    while (n--) {        string s;        cin >> s;        int l = s.length();        if (l > 10) {            cout << s[0] << (l - 2) << s[l - 1] << endl;        } else {            cout << s << endl;        }    }    return 0;}
