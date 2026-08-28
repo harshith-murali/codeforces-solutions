6194dc1b7a9c85fd87708b78188d279a6948dea8
Harshith M <mharshith200@gmail.com>
2026-08-28 18:33:07 +0530

    Add solution for 977A (Wrong Subtraction)
    Submission: https://codeforces.com/contest/977/submission/388678566

---
+++ 977A-Wrong_Subtraction/solution.cpp
@@ -0,0 +1,1 @@
+//// Created by Harshith M on 28/08/26.// #include<iostream>#include<vector>using namespace std; int main() {    int n , k;    cin >> n >> k;     while (k > 0) {        if (n % 10 == 0) {            n = n / 10;        }else {            n -= 1;        }        k--;    }    cout << n << "\n";}
