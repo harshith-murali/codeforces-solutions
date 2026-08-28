575904312231ad9d833ea954f39e448996141870
Harshith M <mharshith200@gmail.com>
2026-08-28 18:49:56 +0530

    Add solution for 110A (Nearly Lucky Number)
    Submission: https://codeforces.com/contest/110/submission/388679754

---
+++ 110A-Nearly_Lucky_Number/solution.cpp
@@ -0,0 +1,1 @@
+//// Created by Harshith M on 28/08/26.// #include<iostream>using namespace std;int main() {    string s;    cin >> s;     int lucky_count = 0;    for (char c : s) {        if (c == '4' || c == '7') {            lucky_count++;        }    }     if (lucky_count == 4 || lucky_count == 7) {        cout << "YES\n";    } else {        cout << "NO\n";    }     return 0;}
